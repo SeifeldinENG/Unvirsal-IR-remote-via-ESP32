11e2b3e6baf896e92b71c6f91236dfd78473486b
SeifeldinENG <shefoeldin@gmail.com>
2026-06-25 19:40:59 +0300

    i dont know

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,119 @@
 #include <ArduinoJson.h>
 
 #define IRPin 15
+#define BUTTON 16
 
 // Json Global Variables
 int counter = 0;
 JsonDocument doc;
 JsonArray signals;
 
-// Structure of devices data
-struct AC {
-  String name, powerUP, powerOFF;
+// Button globar varibals
+bool buttonPrevState;
+
+// Devices enum  
+enum PLACE {
+  LIVING_ROOM,
+  MY_ROOM
 };
 
-struct TV {
-  String name, powerUP, powerOFF;
+enum TYPE {
+  AC,
+  TV,
+  FAN
 };
 
-struct Fan {
-  String name, powerUP, powerOFF;
+enum SELECTOR {
+  IDLE,
+  PLACE,
+  TYPE
 };
+
+PLACE place = LIVING_ROOM;
+TYPE type = AC;
+SELECTOR selector = IDLE;
 
 void setup() {
   Serial.begin(115200);
-  signals = doc["Signals"].to<JsonArray>();
+  pinMode(BUTTON, INPUT_PULLUP);
+  devices = doc["Devices"].to<JsonArray>();
   IrReceiver.begin(IRPin, ENABLE_LED_FEEDBACK);
   Serial.println("ready to receive data!");
+
+}
+
+bool switchButtonClick(bool switchButtonCurrentState) {
+  if (!switchButtonCurrentState && switchButtonPrevState) {
+    delay(50);
+    if (digitalRead(SWITCH)) { 
+      return true;
+    }
+  }
+  switchButtonPrevState = switchButtonCurrentState;
+  return false;
+}
+
+bool sendButtonClick(bool sendButtonCurrentState) {
+  if (!sendButtonCurrentState && sendButtonPrevState) {
+    delay(50);
+    if (digitalRead(SEND)) {
+      return true;
+    }
+  }
+  sendButtonPrevState = sendButtonCurrentState;
+  return false;
+}
+
+void updateDisplay() {
+  switch (selector) {
+
+    case (PLACE): {
+      switch (place) {
+        case (LIVING_ROOM):
+          Serial.println("Living room");
+          place = (place + 1) % 2;
+          break;
+        case (MY_ROOM):
+          Serial.println("My room");
+          place = (place + 1) % 2;
+          break;
+      }
+    } 
+
+    case (TYPE): {
+      switch (type) {
+        case (AC):
+          Serial.println("AC");
+          type = (type + 1) % 3;
+          break;
+        case (TV):
+          Serial.println("TV");
+          type = (type + 1) % 3;
+          break;
+        case (FAN):
+          Serial.println("Fan");
+          type = (type + 1) % 3;
+          break;
+      }
+    }
+    
+    case (IDLE):
+    selector = PLACE;
+    Serial.println("Specify the Location:");
+    break;
+  }
 }
 
 void loop() {
+  if (switchButtonClick(digitalRead(SWITCH)) {
+    updateDisplay();
+    selector = ((selector + 1) % 2) + 1;
+  }
+
+  if (sendButtonClick(digitalRead(SEND))) {
+    // CODE
+  }
+
   if (!IrReceiver.decode()) { return; }
 
   // if (IrReceiver.decodedIRData.protocol == 0 || 
