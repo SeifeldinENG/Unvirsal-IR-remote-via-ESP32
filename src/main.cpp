708f9f4e1c52bad7321c0f9a4d0aa72011bc2687
Seif Eldin <shefoeldin@gmail.com>
2026-06-25 10:10:47 +0300

    Unknown signals detecting

--- src/main.cpp
+++ src/main.cpp
@@ -4,66 +4,84 @@
 
 #define IRPin 15
 
+// Json Global Variables
 int counter = 0;
+JsonDocument doc;
+JsonArray signals;
+
+// Structure of devices data
+struct AC {
+  String name, powerUP, powerOFF;
+};
+
+struct TV {
+  String name, powerUP, powerOFF;
+};
+
+struct Fan {
+  String name, powerUP, powerOFF;
+};
 
 void setup() {
   Serial.begin(115200);
-  delay(300);
+  signals = doc["Signals"].to<JsonArray>();
   IrReceiver.begin(IRPin, ENABLE_LED_FEEDBACK);
   Serial.println("ready to receive data!");
 }
-//TODO: Save the Received IR signal
-//TODO: Prompt the user to enter the name of this signal
-//TODO: println the current database we have
 
 void loop() {
-  static JsonDocument doc;
-  static JsonArray signals = doc["signals"].to<JsonArray>();
+  if (!IrReceiver.decode()) { return; }
 
-  if (IrReceiver.decode()) {
-    if (IrReceiver.decodedIRData.protocol != UNKNOWN && IrReceiver.decodedIRData.protocol != 0) {
-      String firstCode = String(IrReceiver.decodedIRData.decodedRawData, HEX);
-      String secondCode = "";
-      IrReceiver.resume();
+  // if (IrReceiver.decodedIRData.protocol == 0 || 
+  //     IrReceiver.decodedIRData.protocol == UNKNOWN) {
+  //     Serial.println("Unknown or zero signal detected");
+  //     IrReceiver.resume();
+  //     return;
+  // }
 
-      unsigned long sentTime = millis();
-      while (millis() - sentTime < 150) {
-        if (IrReceiver.decode()) {
-          if (IrReceiver.decodedIRData.protocol != UNKNOWN && IrReceiver.decodedIRData.protocol != 0) {
-            secondCode = String(IrReceiver.decodedIRData.decodedRawData, HEX);
-            IrReceiver.resume();
-            break;
-          } 
-        }
-        yield();
-      }
-
-      // Checking for dublicates
-      int i = 0;
-      for (JsonObject entry : signals) {
-        if (entry["Code1_" + String(i)] == firstCode) {
-          IrReceiver.resume();
-          return;
-        }
-        i++;
-      }
-
-      JsonObject newEntry = signals.add<JsonObject>();
-      newEntry["site"] = "IRDevice" + String(counter);
-      newEntry["Code1_" + String(counter)] = firstCode;
-      if (secondCode != "") {
-        newEntry["Code2_" + String(counter)] = secondCode;
-        Serial.println("Two signal codes detected!");
-      } else {
-        newEntry["Code2_" + String(counter)] = "";
-        Serial.println("One signal code detected");
-      }
-      serializeJsonPretty(doc, Serial);
-
-      // Resuming
-      IrReceiver.resume();
-      counter++;
-    }
+  if (IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT) {
+    IrReceiver.resume();
+    return;
   }
+
+  String firstCode = String(IrReceiver.decodedIRData.decodedRawData, HEX);
+  IrReceiver.resume();
+
+  //Waiting for second signal
+  String secondCode = "";
+  unsigned long sentTime = millis();
+  while (millis() - sentTime < 150) {
+    if (IrReceiver.decode()) {
+        secondCode = String(IrReceiver.decodedIRData.decodedRawData, HEX);
+        IrReceiver.resume();
+        break;
+    }
+    IrReceiver.resume();
+    yield();
+  }
+
+  // Checking for dublicates
+  // for (JsonObject entry : signals) {
+  //   if (entry["Code1"].as<String>() == firstCode) {
+  //     IrReceiver.resume();
+  //     return;
+  //   }
+  // }
+
+  JsonObject newEntry = signals.add<JsonObject>();
+  newEntry["site"] = "IRDevice" + String(counter);
+  newEntry["Code1"] = firstCode;
+  if (secondCode != "") {
+    newEntry["Code2"] = secondCode;
+    Serial.println("Two signal codes detected!");
+  } else {
+    newEntry["Code2"] = "";
+    Serial.println("One signal code detected");
+  }
+  serializeJsonPretty(doc, Serial);
+
+  // Resuming
+  IrReceiver.resume();
+  counter++;
 }
 
