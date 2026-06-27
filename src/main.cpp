ea641b4244c430c5e00ffd7c0930ef1392b9cef4
Seif Eldin <shefoeldin@gmail.com>
2026-06-27 13:36:34 +0300

    Serial Monitor selector demo alpha pro max

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include <ArduinoJson.h>
 
 #define IRPin 15
-#define BUTTON 16
+#define SWITCH 16
+#define SEND 17
 
 // Json Global Variables
 int counter = 0;
@@ -11,20 +12,31 @@ JsonDocument doc;
 JsonArray signals;
 
 // Button globar varibals
-bool buttonPrevState;
+bool switchButtonPrevState = true; // input pull up off by default
+bool sendButtonPrevState = true; // input pull up off by default
+
+// Buttons Debounce Variables
+unsigned long switchClickTime = 0;
+unsigned long sendClickTime = 0;
+int DEBOUNCE_MS = 30;
 
 // Global Selectors  
-String place = {"Living Room", " My Room"};
-String type = {"AC", "TV", "Fan"};
-String AC_Key = {"Power UP", "Power OFF", "Plus", "Minus"};
-String TV_Key = {"Power UP", "Power OFF", "OK"};
-String Fan_Key = {"Power UP", "Power OFF", "Speed"};
+String place[] = {"Living Room", " My Room"};
+String type[] = {"AC", "TV", "Fan"};
+String AC_Key[] = {"Power UP", "Power OFF", "Plus", "Minus"};
+String TV_Key[] = {"Power UP", "Power OFF", "OK"};
+String Fan_Key[] = {"Power UP", "Power OFF", "Speed"};
 
 int placeIndex = 0;
-int typeIndex = 0;
-int ACIndex = 0;
+int typeIndex = 0; int ACIndex = 0;
 int TVIndex = 0;
 int FanIndex = 0;
+
+int placeNumber = sizeof(place) / sizeof(place[0]);
+int typeNumber = sizeof(type) / sizeof(type[0]);
+int AC_KeyNumber = sizeof(AC_Key) / sizeof(AC_Key[0]);
+int TV_KeyNumber = sizeof(TV_Key) / sizeof(TV_Key[0]);
+int Fan_KeyNumber = sizeof(Fan_Key) / sizeof(Fan_Key[0]);
 
 enum SELECTOR {
   PLACE,
@@ -44,54 +56,63 @@ DEVICE device = AC;
 // True if assigning a signal
 bool newSignalAssing = false;
 
+// JsonDocument Global Variable
+JsonArray devices = doc["Devices"].to<JsonArray>();
+JsonObject newEntry;
+
 void setup() {
   Serial.begin(115200);
-  pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(SEND, INPUT_PULLUP);
   pinMode(SWITCH, INPUT_PULLUP);
-  devices = doc["Devices"].to<JsonArray>();
   IrReceiver.begin(IRPin, ENABLE_LED_FEEDBACK);
 }
 
-bool switchButtonClick(bool switchButtonCurrentState) {
-  if (!switchButtonCurrentState && switchButtonPrevState) {
-    delay(50);
-    if (digitalRead(SWITCH)) { 
+bool switchButtonClick() {
+  bool state = digitalRead(SWITCH);
+  if (state != switchButtonPrevState) {
+    switchClickTime = millis();
+    switchButtonPrevState = state;
+  }
+
+  if ((millis() - switchClickTime > DEBOUNCE_MS) && !state) {
+      switchClickTime = millis();
       return true;
     }
-  }
-  switchButtonPrevState = switchButtonCurrentState;
   return false;
 }
 
-bool sendButtonClick(bool sendButtonCurrentState) {
-  if (!sendButtonCurrentState && sendButtonPrevState) {
-    delay(50);
-    if (digitalRead(SEND)) {
+bool sendButtonClick() {
+  bool state = digitalRead(SEND);
+  if (state != sendButtonPrevState) {
+    sendClickTime = millis();
+    sendButtonPrevState = state;
+  }
+
+  if ((millis() - sendClickTime > DEBOUNCE_MS) && !state) {
+      sendClickTime = millis();
       return true;
     }
-  }
-  sendButtonPrevState = sendButtonCurrentState;
   return false;
 }
 
 void updateDisplay() {
   switch (selector) {
     case (PLACE):
-      Serial.println(place(placeIndex));
+      Serial.println(place[placeIndex]);
       break;
 
     case (TYPE):
-      Serial.println(type(typeIndex));
+      Serial.println(type[typeIndex]);
       break;
 
     case (KEY):
       switch (device) {
         case (AC):
-          Serial.prinln(AC_Key[ACIndex]);
+          Serial.println(AC_Key[ACIndex]);
           break;
 
         case (TV):
-          Seiral.println(TV_key[TVIndex]);
+          Serial.println(TV_Key[TVIndex]);
           break;
 
         case (FAN):
@@ -102,49 +123,58 @@ void updateDisplay() {
   } 
 }
 
+void waitForRelease() {
+  while (digitalRead(SWITCH) == LOW || digitalRead(SEND) == LOW) {
+    yield();
+  }
+  delay(30); // small delay time after release
+}
+
 void loop() {
   // Waiting for the switch key to be pressed first
   // to enter assgin signal mode
   if (!newSignalAssing) {
     Serial.println("Enter the SWITCH button key to assign new signal.");
-    while (!switchButtonClick(digitalRead(SWITCH))) {
+    while (!digitalRead(SWITCH) == LOW) {
       yield(); // tells the esp32 that its not stuck do not reset
     }
+    waitForRelease();
     newSignalAssing = true;
     updateDisplay();
-    JsonObject newEntry = devices.add<JsonObject>();
+    newEntry = devices.add<JsonObject>();
   }
 
-  if (switchButtonClick(digitalRead(SWITCH)) {
+  if (switchButtonClick()) {
     // Updating the selector indexes
     switch (selector) {
       case (PLACE):
-        placeIndex = (placeIndex + 1) % length(place);
+        placeIndex = (placeIndex + 1) % placeNumber;
         break;
 
       case (TYPE):
-        typeIndex = (typeIndex + 1) % length(type);
+        typeIndex = (typeIndex + 1) % typeNumber;
         break;
       
       case (KEY):
         switch (device) {
           case (AC):
-            ACIndex = (ACIndex + 1) % length(AC_Key);
+            ACIndex = (ACIndex + 1) % AC_KeyNumber;
             break;
 
           case (TV):
-            TVIndex = (TVIndex + 1) % length(TV_Key);
+            TVIndex = (TVIndex + 1) % TV_KeyNumber;
             break;
 
           case (FAN):
-            FanIndex = (FanIndex + 1) % length(Fan_Key);
+            FanIndex = (FanIndex + 1) % Fan_KeyNumber;
             break;
         }
     }
     updateDisplay();
+    waitForRelease();
   }
 
-  if (sendButtonClick(digitalRead(SEND))) {
+  if (sendButtonClick()) {
 
     //  Saving the Choosen variable in the JSON  
     switch (selector) {
@@ -157,6 +187,9 @@ void loop() {
       case (TYPE):
         newEntry["Type"] = type[typeIndex];
         selector = KEY;
+        if (type[typeIndex] == "AC")       device = AC;
+        else if (type[typeIndex] == "TV")  device = TV;
+        else if (type[typeIndex] == "Fan") device = FAN;
         updateDisplay();
         break;
 
@@ -174,6 +207,12 @@ void loop() {
             newEntry["Key"] = Fan_Key[FanIndex];
             break;
         }
+        // Printing the final Json
+        serializeJsonPretty(doc, Serial);
+        Serial.println(" ");
+
+        // Reseting the choices
+        selector = PLACE;
         placeIndex = 0;
         typeIndex = 0;
         ACIndex = 0;
@@ -181,6 +220,7 @@ void loop() {
         FanIndex = 0;
         newSignalAssing = false;
     }
+    waitForRelease();
   }
 }
 
